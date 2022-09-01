b6ca2a900202f669400dbfc834a4d87fffba410c
Ariane Garrett <arigabec@gmail.com>
2022-09-01 17:00:21 -0400

    Firebase work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
-/*import React, { useState } from 'react';
-import { MyComponent } from './components/MyComponent';
-import { TrainerForm } from './components/TrainerForm';*/
+import "./App.css";
+import React, { useState, useEffect } from "react";
 import { PokeCard } from './components/PokeCard';
+import API from "./config/api";
+/*import { MyComponent } from './components/MyComponent';
+import { TrainerForm } from './components/TrainerForm';*/
 
+
+/* AVANCE DE CLASES 
 function App() {
   // const [name, setName] = useState('');
   // const [nameTrainer, setNameTrainer] = useState('');
@@ -13,6 +16,24 @@ function App() {
     // <TrainerForm> </TrainerForm>
     <PokeCard idPokemon={4}></PokeCard>
   );
+}
+
+export default App;*/
+
+function App() {
+  const [trainer, setTrainer] = useState();
+  const getTrainerId = async () => {
+    const trainerReponse = await API.get(`/trainer/3`);
+    const { data } = trainerReponse;
+    setTrainer(data.trainer[0]);
+    console.log(data.trainer);
+  };
+  useEffect(() => {
+    getTrainerId();
+  }, []);
+
+  return <><h1> Esta rotooo </h1></>;
+  // return <>{trainer && <PokeCard idPokemon={trainer.id_pokemon}></PokeCard>}</>;
 }
 
 export default App;
